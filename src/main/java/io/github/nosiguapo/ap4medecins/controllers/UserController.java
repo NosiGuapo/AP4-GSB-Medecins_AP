696c783ba20f3508cfb78ecf369ce9d54cdfb337
NosiGuapo <nosicreations@gmail.com>
2022-02-27 19:20:15 +0100

    User controller

---
+++ src/main/java/io/github/nosiguapo/ap4medecins/controllers/UserController.java
@@ -0,0 +1,39 @@
+package io.github.nosiguapo.ap4medecins.controllers;
+
+import io.github.nosiguapo.ap4medecins.entities.User;
+import io.github.nosiguapo.ap4medecins.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gsb/utilisateurs")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> getAll(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    // Getting the id precised in the url and turning it into a variable
+    public User get(@PathVariable("id") Long id){
+        if (userService.getUserById(id).isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return userService.getUserById(id).get();
+        }
+    }
+}
