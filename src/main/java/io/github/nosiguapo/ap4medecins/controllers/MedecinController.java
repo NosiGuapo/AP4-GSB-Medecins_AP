acab7b69c2c7a839f6f0873b68b70ae0e34e4d97
NosiGuapo <nosicreations@gmail.com>
2022-03-04 19:24:29 +0100

    Basic Bindings for Doctor

--- src/main/java/io/github/nosiguapo/ap4medecins/controllers/MedecinController.java
+++ src/main/java/io/github/nosiguapo/ap4medecins/controllers/MedecinController.java
@@ -28,11 +28,26 @@ public class MedecinController {
     }
 
     // We want to display the informations concerning a specific doctor, we get the mapping of the id suppiled after the default mapping
-    @GetMapping("/{id}")
+    @GetMapping("/id/{id}")
     // Getting the id precised in the url and turning it into a variable
     public Medecin get(@PathVariable("id") Long id){
         // If no doctor is found, return error 404
         return medecinService.getMedecinByid(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("lname/{lname}")
+    public List<Medecin> getByName(@PathVariable("lname") String name){
+        return medecinService.getMedecinByNom(name);
+    }
+
+    @GetMapping("fname/{fname}")
+    public List<Medecin> getByFName(@PathVariable("fname") String name){
+        return medecinService.getMedecinByPrenom(name);
+    }
+
+    @GetMapping("spec/{spec}")
+    public List<Medecin> getBySectorOfActivity(@PathVariable("spec") String spec){
+        return medecinService.getMedecinBySpeciality(spec);
     }
 
     @PostMapping("/delete/{id}")
