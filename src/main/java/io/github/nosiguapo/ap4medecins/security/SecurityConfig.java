b0cc5e93cdfdf5abe5c5527cf94779f650f63ccb
NosiGuapo <nosicreations@gmail.com>
2022-03-29 23:03:46 +0200

    Disabling SecurityConfig.java (test)

--- src/main/java/io/github/nosiguapo/ap4medecins/security/SecurityConfig.java
+++ src/main/java/io/github/nosiguapo/ap4medecins/security/SecurityConfig.java
@@ -1,42 +1,44 @@
-package io.github.nosiguapo.ap4medecins.security;
-
-import io.github.nosiguapo.ap4medecins.filters.AuthFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-// We extend configurerAdapter in order to rewrite some propreties of it
-@Configuration @EnableWebSecurity @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // Beans to pre-existing spring security classes
-    private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().denyAll();
-        // Filters
-        http.addFilter(new AuthFilter(authenticationManagerBean()));
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-}
+//package io.github.nosiguapo.ap4medecins.security;
+//
+//import io.github.nosiguapo.ap4medecins.filters.AuthFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//// We extend configurerAdapter in order to rewrite some propreties of it
+//@Configuration @EnableWebSecurity @RequiredArgsConstructor
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    // Beans to pre-existing spring security classes
+//    private final UserDetailsService userDetailsService;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        System.out.println(http);
+//        http.csrf().disable();
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////        http.authorizeRequests().antMatchers("/login").permitAll();
+//        http.authorizeRequests().anyRequest().permitAll();
+//        // Filters
+//        http.addFilter(new AuthFilter(authenticationManagerBean()));
+//    }
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//}
