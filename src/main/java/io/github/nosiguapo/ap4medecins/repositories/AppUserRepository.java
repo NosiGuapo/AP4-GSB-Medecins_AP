1ac590fad4ed51ef280dcdc079ce74bdfa0a9824
NosiGuapo <nosicreations@gmail.com>
2022-03-30 09:50:04 +0200

    Minor change

--- src/main/java/io/github/nosiguapo/ap4medecins/repositories/AppUserRepository.java
+++ src/main/java/io/github/nosiguapo/ap4medecins/repositories/AppUserRepository.java
@@ -2,7 +2,6 @@ package io.github.nosiguapo.ap4medecins.repositories;
 
 import io.github.nosiguapo.ap4medecins.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
