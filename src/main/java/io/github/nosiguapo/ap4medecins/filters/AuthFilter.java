e8612c5914aa5197d1525cd2fde7d667920063df
NosiGuapo <nosicreations@gmail.com>
2022-04-03 23:59:15 +0200

    Commented shorter-lifetime tokens (for testing purposes)

--- src/main/java/io/github/nosiguapo/ap4medecins/filters/AuthFilter.java
+++ src/main/java/io/github/nosiguapo/ap4medecins/filters/AuthFilter.java
@@ -73,6 +73,7 @@ public class AuthFilter extends UsernamePasswordAuthenticationFilter {
                 .withClaim("lname", detailsUser.getLname())
                 // In milliseconds: 60 sec * 65 (Ap. an hour long token)
                 .withExpiresAt(new Date(System.currentTimeMillis()+ 1000 * 60 * 65))
+//                .withExpiresAt(new Date(System.currentTimeMillis()+ 1000 * 5))
                 .withIssuer(request.getRequestURI())
                 .withClaim(
                         "roles",
